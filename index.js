6b070d7bb36cc25f5d55749b27ece638c3b5631e
Jannis R <mail@jannisr.de>
2017-02-26 18:18:08 +0100

    query routes to find passed stations

--- index.js
+++ index.js
@@ -26,35 +26,70 @@ const walk = (first, opt = {}) => {
 		})
 	}
 
-	const onStations = (stations) => {
+	const onStations = (stations, from) => {
 		for (let station of stations) {
 			if (visited[station.id]) return
 			visited[station.id] = true
 
 			nrOfStations++
 			out.emit('data', station)
-			queue.push(queryAdjacent(station.id))
+			queue.push(queryDepartures(station.id))
+			if (from) queue.push(queryRoutes(from, station.id))
 		}
 		stats()
 	}
 
-	const queryAdjacent = (id) => (cb) => {
+	const queryLocations = (name, from) => (cb) => {
+		nrOfRequests++
+		stats()
+
+		db.locations(name, {addresses: false, poi: false})
+		.then((stations) => {
+			onStations(stations, from)
+			cb()
+		})
+		.catch(cb)
+	}
+
+	const queryDepartures = (id) => (cb) => {
 		nrOfRequests++
 		stats()
 
 		db.departures(id)
-		.then((deps) => Promise.all(deps.map((dep) =>
-			db.locations(dep.direction, {addresses: false, poi: false})
-			.then(onStations)
-		)))
-		.then(() => cb(), cb)
+		.then((deps) => {
+			for (let dep of deps) {
+				const from = dep.station.id
+				queue.push(queryLocations(dep.direction, from))
+			}
+			cb()
+		})
+		.catch(cb)
+	}
+
+	const queryRoutes = (from, to) => (cb) => {
+		nrOfRequests++
+		stats()
+
+		db.routes(from, to, {passedStations: true})
+		.then((routes) => {
+			for (let route of routes) {
+				for (let part of route.parts) {
+					if (!Array.isArray(part.passed)) continue
+					const stations = part.passed.map((dep) => dep.station)
+					onStations(stations)
+				}
+			}
+			cb()
+		})
+		.catch(cb)
 	}
 
 	queue.on('error', (err) => out.emit('error', err))
 	queue.on('end', () => out.end())
+	queue.on('timeout', (_, job) => console.error(job.toString()))
 
 	setImmediate(() => {
-		queue.push(queryAdjacent(first))
+		queue.push(queryDepartures(first))
 		queue.start()
 	})
 
