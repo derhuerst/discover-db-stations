f4d44e408168e8924a305814db95a83c9209482d
Jannis R <mail@jannisr.de>
2017-02-26 15:44:23 +0100

    walk stations

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
 'use strict'
 
-// todo
+const {PassThrough} = require('stream')
+const Queue = require('queue')
+const db = require('db-hafas')
+
+const defaults = {
+	concurrency: 2,
+	timeout: 10 * 1000
+}
+
+const walk = (first, opt = {}) => {
+	opt = Object.assign({}, defaults, opt)
+
+	const out = new PassThrough({objectMode: true})
+	const queue = Queue(opt)
+	const visited = {}
+
+	const onStations = (stations) => {
+		for (let station of stations) {
+			if (visited[station.id]) return
+			visited[station.id] = true
+
+			out.emit('data', station)
+			queue.push(queryAdjacent(station.id))
+		}
+	}
+
+	const queryAdjacent = (id) => (cb) =>
+		db.departures(id)
+		.then((deps) => Promise.all(deps.map((dep) =>
+			db.locations(dep.direction, {addresses: false, poi: false})
+			.then(onStations)
+		)))
+		.then(() => cb(), cb)
+
+	queue.on('error', (err) => out.emit('error', err))
+	queue.on('end', () => out.end())
+
+	setImmediate(() => {
+		queue.push(queryAdjacent(first))
+		queue.start()
+	})
+
+	return out
+}
+
+module.exports = walk
