1c972b179f35d13eca3efb4cfda9040bf5e7bd61
Jannis R <mail@jannisr.de>
2017-04-18 16:53:04 +0200

    report using ansi-diff-stream

--- bin.js
+++ bin.js
@@ -2,6 +2,8 @@
 'use strict'
 
 const minimist = require('minimist')
+const {isatty} = require('tty')
+const differ = require('ansi-diff-stream')
 const esc = require('ansi-escapes')
 const ndjson = require('ndjson')
 
@@ -28,19 +30,28 @@ if (argv.v || argv.version) {
 	process.exit(0)
 }
 
-process.stderr.write('\n')
-const report = ({requests, stations, queued}) => {
-	process.stderr.write(esc.cursorUp(1) + esc.eraseLine + esc.cursorTo(0) + [
-		requests + (requests === 1 ? ' request' : ' requests'),
-		stations + (stations === 1 ? ' station' : ' stations'),
-		queued + ' queued'
-	].join(', ') + '\n')
-}
-
 const data = walk(argv._[0] || 8011102) // Berlin Gesundbrunnen
-if (!argv.s && !argv.silent) data.on('stats', report)
-data.on('error', console.error)
-
 data
+.on('error', console.error)
 .pipe(ndjson.stringify())
 .pipe(process.stdout)
+
+if (!argv.s && !argv.silent) {
+	const clearReports = isatty(process.stderr.fd) && !isatty(process.stdout.fd)
+
+
+	let reporter = process.stderr
+	if (clearReports) {
+		reporter = differ()
+		reporter.pipe(process.stderr)
+	}
+
+	const report = ({requests, stations, queued}) => {
+		reporter.write([
+			requests + (requests === 1 ? ' request' : ' requests'),
+			stations + (stations === 1 ? ' station' : ' stations'),
+			queued + ' queued'
+		].join(', ') + (clearReports ? '' : '\n'))
+	}
+	data.on('stats', report)
+}
